a46a5b7ba76bb323602cfcca310138d85c096f39
kalemakk <kalemarnld@gmail.com>
2021-11-12 17:11:33 +0300

    initial commit

--- src/main/java/com/example/springintegration/PrintService.java
+++ src/main/java/com/example/springintegration/PrintService.java
@@ -1,17 +1,10 @@
 package com.example.springintegration;
 
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 
 public class PrintService {
 
-    //    public Message<String> print(Message<String> message){
-//        System.out.println("message is "+message.getPayload());
-//        int messageNumber = (int) message.getHeaders().get("messageNumber");
-//        return MessageBuilder.withPayload("Sending reply to message "+messageNumber).build();
-//    }
     public void print(Message<String> message) {
-        throw new RuntimeException("this is an error");
-//        System.out.println(message.getPayload());
+        System.out.println(message.getPayload());
     }
 }
--- src/main/java/com/example/springintegration/PrintServiceUpperCase.java
+++ src/main/java/com/example/springintegration/PrintServiceUpperCase.java
@@ -3,6 +3,7 @@ package com.example.springintegration;
 import org.springframework.messaging.Message;
 
 public class PrintServiceUpperCase {
+
     public void print(Message<String> message){
         System.out.println(message.getPayload().toUpperCase());
     }
