ea09cd5c027a8dadb14615f91be31fbfb06a30f6
kalemakk <kalemarnld@gmail.com>
2021-11-15 10:09:17 +0300

    updates

--- src/main/java/com/example/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/example/springintegration/SpringIntegrationApplication.java
@@ -34,7 +34,7 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 
         for (int i =0; i<10; i++){
             Message<?> message = MessageBuilder
-                    .withPayload("This is a String "+i)
+                    .withPayload(i)
                     .build();
             this.printGateWay.print(message);
         }
