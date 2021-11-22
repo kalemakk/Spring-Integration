e2ed3f20baf7601607041bb2e52621c77d3eeedc
kalemakk <kalemarnld@gmail.com>
2021-11-22 12:55:27 +0300

    done

--- src/main/java/com/example/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/example/springintegration/SpringIntegrationApplication.java
@@ -1,15 +1,13 @@
 package com.example.springintegration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+
 
 import java.util.concurrent.ExecutionException;
 
